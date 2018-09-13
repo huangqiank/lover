4dfeee2bd7e4f64b1cf64c541f20cb6a24143060
huangqiank <huang.qiank@husky.neu.edu>
2018-09-14 01:49:59 +0800

    add model

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=32, default='Title')
+    content = models.TextField(null=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
